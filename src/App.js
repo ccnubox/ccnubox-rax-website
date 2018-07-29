3e0217bee66e09907032ebe8eb4eb52bd3d08f8c
zxc0328 <120797887@qq.com>
2018-07-30 07:56:54 +0800

    feat: fix list style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import { createElement, Component, render } from 'rax';
-import View from 'rax-view';
-import Text from 'rax-text';
-import ListView from 'rax-listview';
-import Image from 'rax-image';
-import Link from 'rax-link';
-import SiteServices from '../services/site'
+import { createElement, Component, render } from "rax";
+import View from "rax-view";
+import Text from "rax-text";
+import ListView from "rax-listview";
+import Image from "rax-image";
+import Link from "rax-link";
+// const native = require("@weex-module/test");
+import SiteServices from "../services/site";
 
 let bookTag = {
-  uri: 'http://p9j8ahs4w.bkt.clouddn.com/ccnu-rax-web/book-tag.png'
+  uri: "http://p9j8ahs4w.bkt.clouddn.com/ccnu-rax-web/book-tag.png"
 };
 let arrow = {
-  uri: 'http://p9j8ahs4w.bkt.clouddn.com/ccnu-rax-web/arrow.png'
-}
+  uri: "http://p9j8ahs4w.bkt.clouddn.com/ccnu-rax-web/arrow.png"
+};
 class SitesList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      siteData: null
-    }
+      siteData: []
+    };
   }
   componentWillMount() {
-    SiteServices.getSites().then((siteData) => {
+    SiteServices.getSites().then(siteData => {
       this.setState({ siteData });
-    })
+      // native.changeLoadingStatus(true);
+    });
   }
 
-
-  listItem = (item) => {
+  listItem = (item, index) => {
     return (
-      <View style={styles.item}>
-        <Image style={styles.bookTag} source={bookTag} resizeMode='contain' />
-        <View style={styles.siteText} > {item.site} </View>
-        <Link style={styles.arrowContainer} href={item.url}><Image style={styles.arrow} source={arrow} resizeMode='contain' /></Link>
+      <View
+        style={[
+          styles.item,
+          index === 0 ? styles.first : {},
+          index === this.state.siteData.length - 1 ? styles.last : {}
+        ]}
+      >
+        <Image style={styles.bookTag} source={bookTag} resizeMode="contain" />
+        <View style={styles.siteText}> {item.site} </View>
+        <Link style={styles.arrowContainer} href={item.url}>
+          <Image style={styles.arrow} source={arrow} resizeMode="contain" />
+        </Link>
       </View>
     );
-
-  }
-
-
+  };
 
   render() {
     return (
@@ -48,32 +54,37 @@ class SitesList extends Component {
           dataSource={this.state.siteData}
         />
       </View>
-
     );
   }
 }
 const styles = {
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    marginTop: 45,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgb(239,239,244)',
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(239,239,244,1)"
   },
-
 
   listView: {
     width: 750
   },
   item: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+
     marginBottom: 20,
     height: 100,
-    backgroundColor: 'white',
+    backgroundColor: "white"
+  },
+  first: {
+    marginTop: 45
+  },
+  last: {
+    marginBottom: 90
   },
   bookTag: {
     flex: 1,
@@ -81,8 +92,7 @@ const styles = {
   },
   siteText: {
     fontSize: 30,
-    flex: 2.5,
-
+    flex: 2.5
   },
   arrowContainer: {
     flex: 1,
@@ -90,7 +100,7 @@ const styles = {
   },
   arrow: {
     height: 46
-  },
+  }
 };
 
 export default SitesList;
